f0ed2de357d321c5a437269d1425545b28bf6395
Srinidhi Raghavendra <srinidhiraghavendra@Srinidhis-MacBook-Pro.local>
2019-11-13 21:58:59 -0500

    adding simulator with forwarding

---
+++ SimpleInterlockingWithDataForwarding/cpu.h
@@ -0,0 +1,127 @@
+#include<assert.h>
+#ifndef _APEX_CPU_H_
+#define _APEX_CPU_H_
+#define CC 16
+/**
+ *  cpu.h
+ *  Contains various CPU and Pipeline Data structures
+ *
+ *  Author :
+ *  Gaurav Kothari (gkothar1@binghamton.edu)
+ *  State University of New York, Binghamton
+ */
+
+enum
+{
+  F,
+  DRF,
+  EX1,
+  EX2,
+  MEM1,
+  MEM2,
+  WB,
+  NUM_STAGES
+};
+
+/* Format of an APEX instruction  */
+typedef struct APEX_Instruction
+{
+  char opcode[128];	// Operation Code
+  int rd;		    // Destination Register Address
+  int rs1;		    // Source-1 Register Address
+  int rs2;		    // Source-2 Register Address
+  int rs3;        // Source-3 Register Address
+  int imm;		    // Literal Value
+  int stage_finished; //To track which 
+} APEX_Instruction;
+
+/* Model of CPU stage latch */
+typedef struct CPU_Stage
+{
+  int pc;		    // Program Counter
+  char opcode[128];	// Operation Code
+  int rs1;		    // Source-1 Register Address
+  int rs2;		    // Source-2 Register Address
+  int rs3;        // Source-3 Register Address for STR instructions
+  int rd;		    // Destination Register Address
+  int imm;		    // Literal Value
+  int rs1_value;	// Source-1 Register Value
+  int rs2_value;	// Source-2 Register Value
+  int rs3_value;	// Source-3 Register Value for STR instructions -> can be removed if rd_value is added and rd is used instead of rs3 
+  int buffer;		// Latch to hold some value
+  int mem_address;	// Computed Memory Address
+  int busy;		    // Flag to indicate, stage is performing some action
+  int stalled;		// Flag to indicate, stage is stalled
+  int is_empty;
+} CPU_Stage;
+
+/* Model of APEX CPU */
+typedef struct APEX_CPU
+{
+  /* Clock cycles elasped */
+  int clock;
+
+  /* Current program counter */
+  int pc;
+
+  /* Integer register file */
+  int regs[17];//17th register is the condition code flag - right now used only for the Z flag
+  int regs_valid[17];//17th register is the condition code flag - right now used only for the Z flag
+
+  /* Array of 7 CPU_stage */
+  CPU_Stage stage[7];
+
+  /* Code Memory where instructions are stored */
+  APEX_Instruction* code_memory;
+  int code_memory_size;
+
+  /* Data Memory */
+  int data_memory[1000];
+
+  /* Some stats */
+  int ins_completed;
+
+  /*
+  Forwarding lines
+  Max of 4 needed - in case of STR 3 sources and 1 CC flag
+  */
+ int forwarding_lines_register_address[4];//One each for EX2, MEM1 and MEM2. And the 4th is for CC register
+ int forwarding_lines_data[4];//One each for EX2, MEM1 and MEM2. And the 4th is for CC register
+  
+
+} APEX_CPU;
+
+APEX_Instruction*
+create_code_memory(const char* filename, int* size);
+
+APEX_CPU*
+APEX_cpu_init(const char* filename);
+
+int
+APEX_cpu_run(APEX_CPU* cpu, int no_of_cycles, int flag);
+
+void
+APEX_cpu_stop(APEX_CPU* cpu);
+
+int
+fetch(APEX_CPU* cpu);
+
+int
+decode(APEX_CPU* cpu);
+
+int
+execute1(APEX_CPU* cpu);
+
+int
+execute2(APEX_CPU* cpu);
+
+int
+memory1(APEX_CPU* cpu);
+
+int
+memory2(APEX_CPU* cpu);
+
+int
+writeback(APEX_CPU* cpu);
+
+#endif
